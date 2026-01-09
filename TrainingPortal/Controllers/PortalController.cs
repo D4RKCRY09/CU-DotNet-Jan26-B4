28f87ec4655ceecaddc3236b056d91d9e6568e95
D4RKCRY09 <aarohgaurcuchd1@gmail.com>
2026-01-09 12:43:50 +0530

    Initial Commit

---
+++ TrainingPortal/Controllers/PortalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingPortal.Controllers
+{
+    public class PortalController : Controller
+    {
+        public IActionResult Courses()
+        {
+            return View();
+        }
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+    }
+}
