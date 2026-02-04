748facc1a6b16923c221ca67bb9cc90e4d1e8421
D4RKCRY09 <aarohgaurcuchd1@gmail.com>
2026-02-04 18:21:50 +0530

    Day25 Assignment

---
+++ Day25Assignment/SecureTerminal/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SecureTerminal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string actual_pin = "1234";
+            int i = 4;
+            string pin = string.Empty;
+            string e = string.Empty;
+            while (i > 0)
+            {
+                ConsoleKeyInfo con = Console.ReadKey(true);
+
+                if (con.Key != ConsoleKey.Backspace)
+                {
+                    if (char.IsDigit(con.KeyChar))
+                    {
+                        e += '*';
+                        pin += con.KeyChar;
+
+                    }
+                    else i++;
+                }
+                else
+                {
+                    pin = pin.Remove(pin.Length - 1);
+                    e = e.Remove(e.Length - 1);
+                    i = i + 2;
+                }
+                Console.Clear();
+                Console.Write(e);
+                i--;
+            }
+            Console.WriteLine();
+            if (pin == actual_pin) Console.WriteLine("Pin is Correct");
+            else Console.WriteLine("Pin is Wrong");
+
+        }
+    }
+}
